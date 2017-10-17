ea2952d41f042d3188283463d5f59b3a571919b9
Isaac Yriarte <isaacyriarte@gmail.com>
2017-10-17 15:06:45 -0400

    Se cambian las rutas de sitio para mejor legibilidad

---
+++ src/routes/devsRoutes.php
@@ -0,0 +1,77 @@
+<?php
+
+  use \Psr\Http\Message\ServerRequestInterface as Request;
+  use \Psr\Http\Message\ResponseInterface as Response;
+
+  //API para obtener todos los devs
+  $app->get('/devs', function (Request $request, Response $response) {
+      
+    $devs = Dev::all();
+
+    if (empty($devs)){
+      return "No devs found";
+    }else{
+      return Dev::all()->toJson();
+    }
+
+      //return Dev::all()->toJson();
+    //return "HOLA";
+  });
+
+  //API para obtener un solo dev por su id
+  $app->get('/devs/{id}', function (Request $request, Response $response) {
+    
+    $id = $request->getAttribute('id');
+    try{
+
+      $dev = Dev::findOrFail($id);
+      return $dev->toJson();
+
+    }catch (/* Illuminate\Database\Eloquent\ModelNotFoundException */ Exception $e){
+      return '{"mensaje":"Dev not found"}';
+    }
+
+    //return "HOLA";
+  });
+
+  //Ruta para agregar devs
+  $app->post('/devs', function (Request $request, Response $response) {
+    
+    $data = $request->getParsedBody();
+
+    $dev = new Dev();
+    try{
+
+      if (!isset($data['name'])) throw new Exception("Error Processing Request, name is null or empty", 1);
+      if (!isset($data['focus'])) throw new Exception("Error Processing Request, focus is null or empty", 1);
+      if (!isset($data['hireDate'])) throw new Exception("Error Processing Request, hireDate is null or empty", 1);
+
+      $dev->name = $data['name'];
+      $dev->focus = $data['focus'];
+      $dev->hireDate = $data['hireDate'];
+
+      $dev->save();
+    }catch (Exception $e){
+      return "{\"error\":\"".$e->getMessage()."\"}";
+    }
+
+    return $response->withStatus(201)->getBody()->write('{"mensaje":"Dev agregado exitosamente"}');
+
+  });
+
+  //Ruta para eliminar devs
+  $app->delete('/devs/{id}', function (Request $request, Response $response) {
+    
+    $id = $request->getAttribute('id');
+    try{
+
+      $dev = Dev::findOrFail($id);
+      $dev->delete();
+      return $response->withStatus(200);
+
+    }catch (/* Illuminate\Database\Eloquent\ModelNotFoundException */ Exception $e){
+      return '{"mensaje":"Dev not found, excepcion: '.$e->getMessage().'"}';
+    }
+
+    //return "HOLA";
+  });
