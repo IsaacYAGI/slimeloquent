d7d4eaac9712a94f4960699023933017510f16d7
Isaac Yriarte <isaacyriarte@gmail.com>
2017-10-23 14:44:56 -0400

    Se agregan las rutas para obtener todos los datos de un usuario y sus posts. Tambien otra ruta para obtener todos los usuarios y todos los posts por cada uno.

--- src/routes/usersRoutes.php
+++ src/routes/usersRoutes.php
@@ -4,6 +4,7 @@
   use \Psr\Http\Message\ResponseInterface as Response;
 
   //API para obtener todos los users
+
   $app->get('/users', function (Request $request, Response $response) {
       
     $users = User::all();
@@ -17,6 +18,8 @@
       //return Dev::all()->toJson();
     //return "HOLA";
   });
+
+  //API para los posts de un usuario
 
   $app->get('/userPosts/{id}', function (Request $request, Response $response) {
     
@@ -32,6 +35,55 @@
 
       //return Dev::all()->toJson();
     //return "HOLA";
+  });
+
+  //API para obtener los datos de un usuario y todos los posts a los que esta asociado
+
+  $app->get('/userAndPosts/{id}',function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+
+    try{
+
+      $user = User::findOrFail($id);
+      
+      $data = [];
+
+      $user->posts;
+
+      $data["usuario"] = $user;
+
+      $response->getBody()->write(json_encode($data));
+
+      return $response;
+
+    }catch (/* Illuminate\Database\Eloquent\ModelNotFoundException */ Exception $e){
+      return '{"mensaje":"User not found"}';
+    }
+
+  });
+
+  //API para obtener todos los usuarios y los posts que posee cada uno
+
+  $app->get('/usersAndPosts',function (Request $request, Response $response) {
+    //$id = $request->getAttribute('id');
+
+    try{
+      $users = User::all();
+      
+      foreach ($users as $usuario) {
+        $usuario->posts;
+      }
+
+      $data["usuarios"] = $users;
+
+      $response->getBody()->write(json_encode($data));
+
+    }catch(Exception $e){
+      //echo $e->getMessage();
+      return '{"mensaje":"Error: '.$e->getMessage().'"}';
+
+    }
+
   });
 
 
